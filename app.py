a10e6f93fe10d99456286d8d99dda2ae18aabef2
saksham1598-hash <dinamicsaksham@gmail.com>
2025-04-18 01:46:22 -0500

    Update app.py
    removed commented section -old version

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-# from config import RAG_CONFIG
-# from rag.rag_chain import RAGChain
-# from utils.logger import get_logger
-# import sys
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-# logger = get_logger()
-
-# def main():
-#     if len(sys.argv) < 2:
-#         print("Usage: python app.py '<your question>'")
-#         return
-
-#     question = sys.argv[1]
-#     rag_pipeline = RAGChain(RAG_CONFIG)
-
-#     docs = rag_pipeline.retrieve_documents(question)
-#     # print("=" * 100)
-#     # print("Retrieved Documents:\\n")
-#     # print(docs)
-#     # print("=" * 100)
-
-#     answer = rag_pipeline.answer_question(question)
-#     print("\\n" + "*" * 100)
-#     print("Answer:\\n")
-#     print(answer)
-#     # print("*" * 100)
-
-# if __name__ == "__main__":
-#     main()
-
 from config import RAG_CONFIG
 from rag.rag_chain import RAGChain
 from utils.logger import get_logger
